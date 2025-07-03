f6525a8272c90e21cee07dd09a3ec0fc3fecedc3
Jake0000-why <jakeybear777@gmail.com>
2025-07-03 13:49:07 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+});
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.listen(port, () =>{
+  console.log(`Server is running at http://localhost:${port}`);
+})
